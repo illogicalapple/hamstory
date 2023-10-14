6efa5f697bc2fe56760ac49c3c44cd8deb151092
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-10-14 07:50:34 -0500

    transition

--- src/menu/main_menu.gd
+++ src/menu/main_menu.gd
@@ -1,1 +1,10 @@
 extends Control
+
+func to_screen(coordinates: Vector2i):
+	var tween: Tween = create_tween()
+	tween.tween_property($Camera2D, "position", Vector2(coordinates.x * 1920, coordinates.y * 1080), 1.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+
+
+
+func _on_start_pressed():
+	to_screen(Vector2(1, 1))
