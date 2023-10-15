b5dd8a4dc9fc9d3848c81b0b5e3ccf2b43047b60
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-10-14 19:18:15 -0500

    implement quit
    and some readability stuff

--- src/components/select_button.gd
+++ src/components/select_button.gd
@@ -2,12 +2,17 @@ extends Button
 
 var height_or_whatever: float = 0:
 	set(new_value):
+		# this controls the height of the snazzy rectangle thing
+		# i made it a setter just because i felt like it :)
 		height_or_whatever = new_value
 		$Select.set_size(Vector2($Select.get_size().x, height_or_whatever))
 
-var focus_state: bool = false:
+var focus_state: bool = false: # consider adding gamepad support (it's a setter to make that easier)
+	# how do i lower the indentation levels here :thonk:
 	set(new_value):
 		focus_state = new_value
+		
+		# this controls the height of the snazzy rectangel thing
 		if focus_state:
 			var tween: Tween = create_tween()
 			tween.tween_property(self, "height_or_whatever", 25, 0.1)
@@ -16,12 +21,11 @@ var focus_state: bool = false:
 			tween.tween_property(self, "height_or_whatever", 0, 0.1)
 
 func _ready():
+	# this controls the height of the snazzy rectangle thing
 	($Select as ColorRect).set_size(Vector2(get_size().x - 40, 0))
 	$Select.set_anchors_preset(Control.PRESET_CENTER_BOTTOM)
 	$Select.position -= Vector2($Select.get_size().x / 2, 30)
 
-func _on_mouse_entered():
-	focus_state = true
-
-func _on_mouse_exited():
-	focus_state = false
+# mouse logic
+func _on_mouse_entered(): focus_state = true
+func _on_mouse_exited(): focus_state = false
--- src/menu/main_menu.gd
+++ src/menu/main_menu.gd
@@ -1,10 +1,54 @@
 extends Control
 
-func to_screen(coordinates: Vector2i):
+var transition_list: Array[Vector2i] = [Vector2i(0, 0)] # this array stores the coordinates so you can chain esc
+var active_tweens: Array[Tween] = [] # this array stores the tweens to kill later
+var esc_exception: Callable
+var is_esc_exception: bool = false
+
+func to_screen(coordinates: Vector2i, push: bool = true) -> Vector2i:
+	# some logging stuff:
+	for tween in active_tweens: tween.kill() # kill all current tweens
+	if push: transition_list.append(coordinates) # push to coordinates
+	
+	# movement stuff:
 	var tween: Tween = create_tween()
+	active_tweens.append(tween) # log tween so it can be killed later
 	tween.tween_property($Camera2D, "position", Vector2(coordinates.x * 1920, coordinates.y * 1080), 1.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	
+	# print(transition_list) # ROP
+	
+	return coordinates
+
+func _on_start_pressed() -> Vector2i:
+	return (to_screen(Vector2i(1, 1)) as Vector2i)
+
+func _input(event: InputEvent) -> void:
+	# THIS IS THE ESCAPE BUTTON CODE
+	# change to functional code if we need another input
+	
+	# anti-conditions
+	if not event.is_action_pressed("ui_back"): return
+	
+	# check for escape override
+	if is_esc_exception:
+		esc_exception.call()
+		return
+	
+	if len(transition_list) < 2: return
+	
+	transition_list.pop_back()
+	to_screen(transition_list.back(), false)
 
 
+func _on_quit_pressed():
+	$MainScreen/QuitConfirm.show()
+	is_esc_exception = true
+	esc_exception = Callable($MainScreen/QuitConfirm, "hide")
 
-func _on_start_pressed():
-	to_screen(Vector2(1, 1))
+func _on_cancel_pressed():
+	is_esc_exception = false
+	$MainScreen/QuitConfirm.hide()
+
+
+func quit_game_frfr():
+	get_tree().quit()
